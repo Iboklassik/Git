2b68b6f33f5b32de218022f6199d98049e91e9aa
iboklassik <iboklassik@gmail.com>
2021-10-03 23:05:04 -0700

    added pyton files

---
+++ task.py
@@ -0,0 +1,16 @@
+a=input( "a verileni daxil et: " )
+b=input("b verileni daxil et: "  )
+
+n1 = int(a)
+n2 = int(a+a)
+n3 = int(a+a+a)
+
+print(n1+n2+n3)
+
+
+x1 = int(b)
+x2 = int(b+b)
+x3 = int(b+b+b)
+print(x1+x2+x3)
+
+
